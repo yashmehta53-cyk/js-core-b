5268958aa608eb4a3c5d742e1e967a41425d30a5
yashmehta53-cyk <ymehta7251@gmail.com>
2026-02-09 10:00:25 +0530

    Java Code

--- script.js
+++ script.js
@@ -1,38 +1,126 @@
-const para = document.getElementsByClassName('para');
+// const para = document.getElementsByClassName('para');
 
-para[0].textContent = "THIS is now updated paragraph";
-para[1].style.color = "green";
+// para[0].textContent = "THIS is now updated paragraph";
+// para[1].style.color = "green";
 
-console.log(para);
+// console.log(para);
 
-const para2 = document.querySelectorAll('.para');
+// const para2 = document.querySelectorAll('.para');
 
-para2[0].textContent = "THIS is now updated paragraph";
-para2[1].style.color = "green";
+// para2[0].textContent = "THIS is now updated paragraph";
+// para2[1].style.color = "green";
 
-para2.forEach((ele) => {
-    console.log(ele.textContent);
-});
+// para2.forEach((ele) => {
+//     console.log(ele.textContent);
+// });
 
-const button = document.querySelector('.btn');
+// const button = document.querySelector('.btn');
 
-function message() {
-    alert("You have clicked the button");
-}
+// function message() {
+//     alert("You have clicked the button");
+// }
 
-button.addEventListener('click', message);
+// button.addEventListener('click', message);
 
-const input = document.querySelector('.input');
+// const input = document.querySelector('.input');
 
-input.addEventListener('keyup', function (event) {
-    console.log(event.key);
-});
-const form = document.getElementById("myForm");
+// input.addEventListener('keyup', function (event) {
+//     console.log(event.key);
+// });
+// const form = document.getElementById("myForm");
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
+// form.addEventListener("submit", function (e) {
+//     e.preventDefault();
 
-    console.log("Form submitted ");
+//     console.log("Form submitted ");
 
-    alert("Submit event triggered!");
-});
+//     alert("Submit event triggered!");
+// });
+
+// <form id="myForm">
+//     <input type="text" placeholder="Enter name">
+//     <button type="submit">Submit</button>
+// </form> 
+
+// // const para = document.getElementsByClassName('para');
+
+// // para[0].textContent = "THIS is now updated paragraph";
+// // para[1].style.color = "green";
+
+// // console.log(para);
+
+
+// // const para2 = document.querySelectorAll('.para');
+
+// // para2[0].textContent = "THIS is now updated paragraph";
+// // para2[1].style.color = "green";
+
+// // para2.forEach((ele) => {
+// //     console.log(ele.textContent);
+// // });
+
+
+// // const button = document.querySelector('.btn');
+
+// // function message() {
+// //     alert("You have clicked the button");
+// // }
+
+// // button.addEventListener('click', message);
+
+// // const input = document.querySelector('.input');
+
+// // input.addEventListener('keyup', function (event) {
+// //     console.log(event.key);
+// // });
+// // const form = document.getElementById("myForm");
+
+// // for(m.addEventListener("submit", function (e) {
+// //     e.preventDefault(); 
+
+// //     console.log("Form submitted ");
+
+// //     alert("Submit event triggered!");
+// // });
+
+// // <form id="myForm">
+// //     <input type="text" placeholder="Enter name">
+// //     <button type="submit">Submit</button>
+// // </foci
+// // const p=new Promise(function(resolve,reject){
+// //     setTimeout(()=>{
+// //         let done=false;
+// //         if(done){
+// //             resolve({name:"alex",age:34})
+// //         }else{
+// //             reject("network issue")
+// //         }
+// //     },5000)
+// // })
+// // p.then((data)=>{
+// //     console.log("resolved",data)
+// // }).catch((err)=>{
+// //     console.log("rejected",err)
+// // }).finally(()=>{
+// //     console.log("finally block")
+// // })
+// // function doHomework(){
+// //     const p=new Promise(function(resolve,reject){
+// //         setTimeout(()=>{
+// //             let done=true;
+
+// //             if(done){
+// //                 console.log=("homewokr is done")
+// //                 resolve("homework is complete")
+// //             }else{
+// //                 reject("homework is not done")
+// //             }
+// //         },5000)
+// //     })
+// //     return p
+// // }
+// // doHomework().then((data)=>{
+// //     console.log(data)
+// //     return goTopplayground()
+// // }).then((data)=>)
+// // return network
